7cbb58bad0ea278058d0ff1e10b8dc4b0dfd27fd
Steve Morton <taeo920@gmail.com>
2024-08-02 14:47:32 -0400

    Check in

---
+++ app/components/gallery/index.js
@@ -0,0 +1,41 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
+import './style.scss';
+
+export default function Gallery(props) {
+  useEffect(() => {
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: '#' + props.galleryID,
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+    });
+    lightbox.init();
+
+    return () => {
+      lightbox.destroy();
+      lightbox = null;
+    };
+  }, []);
+
+  return (
+    <div className="l-grid gallery" id={props.galleryID} data-columns="3">
+      {props.images.map((image, index) => (
+        <a
+          className="gallery__item"
+          href={image.largeURL}
+          data-pswp-width={image.width}
+          data-pswp-height={image.height}
+          key={props.galleryID + '-' + index}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={image.thumbnailURL} alt="" />
+        </a>
+      ))}
+    </div>
+  );
+}
+  
--- app/page.js
+++ app/page.js
@@ -1,95 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import Card from './components/card';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="page page--home">
+      <section className="hero">
+        <div className="hero__container">
+          <h2 className="hero__heading">Dope AF Tattoos</h2>
+          <span className="hero__sub-heading">Finksburg, Maryland</span>
         </div>
-      </div>
+        <div className="hero__background">
+          <Image className="hero__image" src="/shop.jpg" alt="" width="1440" height="810" />
+        </div>
+      </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <section className="l-section">
+        <div className="l-section__container">
+          <h2 className="l-section__heading">Artists</h2>
+          <div className="l-grid" data-columns="2">
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
