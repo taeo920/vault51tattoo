f4c8d2aa67052552471c130aa85bc081fc5847f1
Steve Morton <steve.morton@tealmedia.com>
2025-12-28 14:09:44 -0500

    Added aaliyah, destiny, hero video, artist layout improvements

--- app/page.js
+++ app/page.js
@@ -11,14 +11,14 @@ export default function Home() {
           <span className="hero__sub-heading">Finksburg, Maryland</span>
         </div>
         <div className="hero__background">
-          <Image className="hero__image" src="/shop.jpg" alt="" width="1440" height="810" />
+          <video className="hero__video is-visible" src="/shop.mp4" poster="/shop.jpg" autoPlay muted loop width="1440" height="810"></video>
         </div>
       </section>
 
       <section className="l-section">
         <div className="l-section__container">
           <h2 className="l-section__heading">Artists</h2>
-          <div className="l-grid" data-columns="2">
+          <div className="l-grid">
             {Artists.map((artist, index) => (
               <Card artist={artist} key={index} />
             ))}
